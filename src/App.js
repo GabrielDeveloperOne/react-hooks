15f33f648bf8576296a22d3cd5d735115895786a
Gabriel <gabriel.developer@outlook.com>
2020-05-05 01:21:59 -0300

    criando create do crud

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React, { useState } from 'react';
 import UserTable from './tables/UserTable';
+import AddUserForm from './forms/AddUserForm';
 
 const App = () => {
 
   const userData = [
-    {id: 1, name: 'Gabriel', username: 'majorbill'}
+    {id: 1, name: 'Gabriel', username: 'majorbill'},
+    {id: 2, name: 'Maria', username: 'mariagracinha'},
+    {id: 3, name: 'Olegario', username: 'olegarizinho'}
   ]
+
+  const [users, setUsers] = useState(userData)
+
+  const addUser = user => {
+    user.id = users.length + 1
+    setUsers([...users, user])
+  }
 
   return (
     <div className="container">
@@ -13,10 +23,11 @@ const App = () => {
         <div className="flex-row">
           <div className="flex-large">
             <h2>Add user</h2>
+            <AddUserForm addUser={addUser} />
           </div>
           <div className="flex-large">
             <h2>View user</h2>
-            <UserTable />
+            <UserTable users={users}/>
           </div>
         </div>
     </div>
